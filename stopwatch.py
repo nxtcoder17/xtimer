2b623330fc069be0341211321c688b09f709476c
nxtcoder17 <nxtcoder17@gmail.com>
2020-10-29 10:17:57 +0530

    Added Pause Stopwatch feature on <Ctrl-C>

--- stopwatch.py
+++ stopwatch.py
@@ -2,9 +2,16 @@ import time
 
 from libs import getPrintString, printIt
 
+allowedInterrupt = 1
 totalTime = 0
 while True:
-    printIt(getPrintString(totalTime))
-    time.sleep(1)
-    totalTime += 1
+    try:
+        printIt(getPrintString(totalTime))
+        time.sleep(1)
+        totalTime += 1
+    except KeyboardInterrupt:
+        printIt("Stopwatch Paused ...")
+        x = input()
+        if x:
+            continue
 
